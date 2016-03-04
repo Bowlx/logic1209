7ab6647960b9a7a1d8123e463fb209fc021cfa19
Дмитрий Емцов <krentorr@gmail.com>
2016-03-04 20:11:03 +0300

    1025. Демократия в опасности

---
+++ logic1025/logic1025/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace logic1025
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Result = 0;
+            int am = int.Parse(Console.ReadLine());
+            string[] str = Console.ReadLine().Split(' ');
+            int[] array = new int[str.Length];
+            for (int i = 0; i < am; ++i)
+            
+                array[i] = int.Parse(str[i]);
+                
+            
+
+            Array.Sort(array);
+            double num = Math.Ceiling(array.Length / 2.0);
+
+            for (int i = 0; i < num; ++i)
+            {
+                Result += (Math.Ceiling(array[i] / 2.0));
+            }
+            Console.WriteLine(Result);
+        }
+    }
+}
